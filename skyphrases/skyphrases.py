064fadc130ba1e204f2049728aa364c2153a3c08
Artur <31221055+Air-t@users.noreply.github.com>
2019-03-21 10:44:38 +0100

    Logic updated

--- skyphrases/skyphrases.py
+++ skyphrases/skyphrases.py
@@ -3,7 +3,7 @@ def how_many_phrases_are_valid():
     returns count of valid lines."""
     with open('input.txt', 'r') as f:
         phrases = [line.split() for line in f.readlines()]
-        return sum([1 if len(line) != len(set(line)) else 0 for line in phrases])
+        return sum([0 if len(line) != len(set(line)) else 1 for line in phrases])
 
 
 if __name__ == '__main__':
